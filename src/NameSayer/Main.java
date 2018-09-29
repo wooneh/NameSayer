c253a56d32dbb7985787e2eda0b61716e6c7b1c7
clyanide <clayton.lan18@gmail.com>
2018-09-30 10:36:24 +1300

    Added basic homescreen

--- src/NameSayer/Main.java
+++ src/NameSayer/Main.java
@@ -10,7 +10,7 @@ public class Main extends Application {
 
 	@Override
 	public void start(Stage stage) throws Exception {
-		Parent root = FXMLLoader.load(getClass().getResource("NameSayer.fxml"));
+		Parent root = FXMLLoader.load(getClass().getResource("HomeScreen.fxml"));
 		Scene scene = new Scene(root);
 
 		stage.setTitle("NameSayer");
