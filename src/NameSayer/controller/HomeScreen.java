27348004dde42fa4b51c95f549eb3205ddabd715
clyanide <clayton.lan18@gmail.com>
2018-10-06 16:21:29 +1300

    updated home screen

--- src/NameSayer/controller/HomeScreen.java
+++ src/NameSayer/controller/HomeScreen.java
@@ -3,22 +3,21 @@ package NameSayer.controller;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 
+import java.io.File;
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
-public class HomeScreen {
+public class HomeScreen implements Initializable {
 	@FXML Button startButton;
 
-	public void initialize() {
-    	startButton.setOnAction(event -> {
-    		try {
-				startButton.getScene().setRoot(new FXMLLoader(getClass().getResource("/NameSayer/view/InputNames.fxml")).load());
-			} catch (IOException e) {
-    			e.printStackTrace();
-			}
-    	});
-	}
+	@FXML
+	ImageView imageView;
 
 	@FXML
 	public void helpButtonAction(ActionEvent event) {
@@ -27,5 +26,21 @@ public class HomeScreen {
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+	}
+
+	@Override
+	public void initialize(URL url, ResourceBundle resourceBundle) {
+		File file = new File("SOFTENG-206-Assignment-3/src/study.jpg");
+		System.out.println(file);
+		Image image = new Image(file.toURI().toString());
+		imageView.setImage(image);
+
+		startButton.setOnAction(event -> {
+			try {
+				startButton.getScene().setRoot(new FXMLLoader(getClass().getResource("/NameSayer/view/InputNames.fxml")).load());
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		});
 	}
 }
